2600ee2c0075b1e2be5edefe8aa975f9cc120e33
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2021-12-22 18:00:46 +0400

    Add hashing to authentication

--- src/models/user.js
+++ src/models/user.js
@@ -4,58 +4,67 @@ const bcrypt = require("bcryptjs");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-		validate(email) {
-			if (!validator.isEmail(email)) {
-				throw new Error("Email is not valid!");
-			}
-		},
-	},
-	password: {
-		type: String,
-		trim: true,
-		required: true,
-		minLength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes("password")) {
-				throw new Error("Your password is too common");
-			}
-		},
-	},
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate(email) {
+      if (!validator.isEmail(email)) {
+        throw new Error("Email is not valid!");
+      }
+    },
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    minLength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Your password is too common");
+      }
+    },
+  },
 });
 userSchema.statics.findAndCompareUser = async (email, password) => {
-	try {
-		const user = await User.findOne({ email });
-		if (!user) {
-			throw new Error("Unable to login!");
-		}
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Unable to login!");
+    }
 
-		const matched = await bcrypt.compare(password, user.password);
-		if (!matched) {
-			throw new Error("Unable to login!");
-		}
-		return user;
-	} catch (er) {
-		// return(er)
-	}
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+      throw new Error("Unable to login!");
+    }
+    return user;
+  } catch (er) {
+    // return(er)
+  }
 };
 
-userSchema.pre("save", async function (next) {
-	const user = this;
-	if (user.isModified("password")) {
-		user.password = await bcrypt.hash(user.password, 8);
-	}
-	next();
-});
+userSchema.pre('save', async function (next) {
+    const user = this
+  try {
+
+  
+    if (user.isModified('password')) {
+      console.log('hello')
+        user.password = await bcrypt.hash(user.password, 8)
+      console.log(user.password)  
+  }
+    }catch(err) {
+    console.log(err)
+  }
+    
+    next()
+})
 
 const User = mongoose.model("User", userSchema);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,32 +3,29 @@ const User = require("../models/user");
 
 const router = new express.Router();
 
-router.post("/signup", async (req, res) => {
-	const user = new User(req.body);
-	const foundUser = await User.findOne({ email: req.body.email });
-	if (foundUser) {
-		console.log(foundUser);
-		return res.status(400).send(req.body.email);
-	}
+router.post("/register", async (req, res) => {
+  const user = new User(req.body)
+  console.log(user)
+  try {
 
-	try {
-		await user.save();
-		res.status(201).send({ user });
-	} catch (e) {
-		res.status(400).send(e);
-	}
+    await user.save();
+    res.status(201).send({ user });
+  } catch (e) {
+    res.status(402).send("error");
+  }
 });
 
 
-router.post('/login', async (req,res) => {
-		const user = await User.findAndCompareUser(req.body.email, req.body.password)
-		if(!user){
-			return res.status(404).send()
-		}
-		res.send({user})
-
-
-})
+router.post("/login", async (req, res) => {
+  const user = await User.findAndCompareUser(
+    req.body.email,
+    req.body.password
+  );
+  if (!user) {
+    return res.status(404).send();
+  }
+  res.status(201).send({ user });
+});
 
 
 module.exports = router;
