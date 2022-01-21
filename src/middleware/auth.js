b6bcbb2094db9e07196e4bea340d6662dca86dad
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-01-21 22:47:11 +0400

    Add auth middleware and logout route

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("auth-token").replace('Bearer ', '')
+    console.log(token)
+    const verified = jwt.verify(token, process.env.JWT_KEY)
+    const user = await User.findOne({ _id: verified._id, 'tokens.token': token });
+    console.log(user)
+    if (!user) {
+      throw new Error()
+    }
+    req.user = user
+    req.token = token
+
+    next()
+  } catch (er) {
+    res.status(402).send("Pleace authenticate")
+  }
+}
+
+module.exports = auth
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const jwt = require('jsonwebtoken');
 const User = require("../models/user");
+const auth = require('../middleware/auth')
 
 const router = new express.Router();
+
+
 
 router.post("/register", async (req, res) => {
   const user = new User(req.body)
@@ -26,9 +29,25 @@ router.post("/login", async (req, res) => {
   }
 
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY)
-  user.tokens = user.tokens.concat({token})
+  user.tokens = user.tokens.concat({ token })
   await user.save()
   res.status(201).send({ user });
 });
+
+
+router.post('/logout', auth, async (req, res) => {
+ try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 
 module.exports = router;
