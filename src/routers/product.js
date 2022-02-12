902d775f8e003bd886b46fadf97c383496a9f5dd
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-02-12 20:57:40 +0400

    Add basic admin middleware and use it in product router

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const Product = require("../models/product");
 
+// Middlewares
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+
 const router = new express.Router();
 
-router.post("/add", async (req, res) => {
+router.post("/create", auth, admin, async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.status(201).send({ product });
 });
 
+// Get all products
 router.get("/", async (req, res) => {
-  const products = await Product.find();
-  res.status(201).send({ products });
+  try {
+    const products = await Product.find();
+    res.status(201).send({ products });
+  } catch (err) {
+    res.status(404).send({ err });
+  }
 });
 
-router.delete("/remove", (req, res) => {
+router.delete("/:id", auth, admin, (req, res) => {
   try {
     const product = Product.findOneAndDelete({ _id: req.body.id });
     res.status(201).send();
