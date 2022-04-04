e84ec6b29c1a5149be755f75b1ea9b4e8e6da171
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-04-04 23:57:45 +0400

    Allow cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routers/user");
 const productRouter = require("./routers/product");
 const categoryRouter = require("./routers/category");
@@ -6,6 +7,15 @@ const bodyParser = require("body-parser");
 require("./db/mongoose");
 
 const app = express();
+
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
 
 app.use(express.json());
 app.use(bodyParser.json());
