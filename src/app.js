caaaf777a61237476adc1f5ac118941620dc407c
kaxa-imerlishvili <kakha.imerlishvili@edu.btu.ge>
2022-06-03 22:32:38 +0400

    Add cors package and use it

--- src/app.js
+++ src/app.js
@@ -4,18 +4,12 @@ const productRouter = require("./routers/product");
 const categoryRouter = require("./routers/category");
 const checkoutRouter = require("./routers/checkout");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("./db/mongoose");
 
 const app = express();
 
-const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
-
-app.use(allowCrossDomain);
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/user", userRouter);
